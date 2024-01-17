0eea6ec32db0aa768161c6d294073dc211617622
jonasarce1 <jonasarceg@gmail.com>
2024-01-17 18:48:25 +0100

    getcontactos

--- gql/Query.ts
+++ gql/Query.ts
@@ -1,5 +1,40 @@
-
+import { ContactoModelType, ContactoModel } from "../db/ContactoDB.ts"
+import mongoose from "mongoose";
+import { GraphQLError } from "graphql";
 
 export const Query = {
-    hello: () => "Hello world!",
+    getContactos: async(): Promise<Array<{nombre: string, apellido1: string, apellido2: string, dni: string}>> => {
+        const contactos = await ContactoModel.find().exec();
+        const contactosCortos = contactos.map(function (contacto) {
+            return {
+                nombre: contacto.nombre,
+                apellido1: contacto.apellido1,
+                apellido2: contacto.apellido2,
+                dni: contacto.dni
+            }
+        })
+        return contactosCortos;
+    },
+
+    getContacto: async(_:unknown, args:{dni:string}): Promise<ContactoModelType> => {
+        try{
+            const contacto = await ContactoModel.findOne({dni:args.dni}).exec();
+
+            if(!contacto){
+                throw new GraphQLError("No se pudo encontrar el contacto con ese dni");
+            }
+
+            return contacto;
+        }catch(error){
+            if(error instanceof mongoose.Error.ValidationError){
+                const validationErrors = Object.keys(error.errors).map(
+                    (key) => error.errors[key].message
+                );
+                throw new GraphQLError("Error de validacion: " + validationErrors.join(", "));
+            }else{
+                throw new GraphQLError(error.message);
+            }
+        }
+    }
 };
+
--- gql/schema.ts
+++ gql/schema.ts
@@ -1,6 +1,21 @@
 export const typeDefs = `#graphql
+    type ContactoGQL {
+        dni: String!,
+        nombre: String!,
+        apellido1: String!,
+        apellido2: String!,
+        email: String!,
+        cp: String!,
+        iso: String!, 
+        ciudad: String,
+        pais: String,
+        hora: String,
+        condicionesMeteo: String
+    }
+
     type Query {
-        hello: String!
+        getContactos: [ContactoGQL!]!,
+        getContacto: ContactoGQL!
     },
 
     type Mutation {
