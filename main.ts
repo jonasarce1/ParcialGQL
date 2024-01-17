05c2447d08dcd9a01d06aa4cddd631bc93f776a2
jonasarce1 <jonasarceg@gmail.com>
2024-01-17 18:07:11 +0100

    mongo ready

--- main.ts
+++ main.ts
@@ -7,16 +7,15 @@ import { Query } from "./gql/Query.ts";
 import { Mutation } from "./gql/Mutation.ts";
 
 
-/*const MONGO_URL = Deno.env.get("MONGO_URL");
+const MONGO_URL = Deno.env.get("MONGO_URL");
 
 if (!MONGO_URL) {
   console.log("No mongo URL found");
   Deno.exit(1);
 }
 
-await mongoose.connect(MONGO_URL);*/
+await mongoose.connect(MONGO_URL);
 
-//server vacio
 const server = new ApolloServer({
   typeDefs,
   resolvers: {
